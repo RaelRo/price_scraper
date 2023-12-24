8f3845b5203bbdab2b6b829accb33941a752bf71
RaelRo <67835815+RaelRo@users.noreply.github.com>
2023-12-24 14:38:49 +0100

    Update playstorePriceScraping.py

--- playstorePriceScraping.py
+++ playstorePriceScraping.py
@@ -1,30 +1,28 @@
 import re
 from bs4 import BeautifulSoup
 import requests
-import os
-import time
 import datetime
 
 key = 0
 def convertTuple(tuple):
-    string = ''.join(map(str,tuple))
+    string = ''.join(map(str, tuple))
     return string
 
 
-urls = ["https://play.google.com/store/books/details/Rod_Dreher_Live_Not_by_Lies?id=JqfiDwAAQBAJ",
-        "https://play.google.com/store/books/details/Timothy_Keller_Romans_1_7_For_You?id=wwPPDwAAQBAJ",
-        "https://play.google.com/store/books/details/Timothy_Keller_Romans_8_16_For_You?id=uwTPDwAAQBAJ",
-        "https://play.google.com/store/books/details/Timothy_Keller_Galatians_For_You?id=7QTPDwAAQBAJ",
-        "https://play.google.com/store/books/details/Timothy_Keller_Judges_For_You?id=XQTPDwAAQBAJ",
-        "https://play.google.com/store/books/details/Timothy_Keller_On_Marriage?id=i2asDwAAQBAJ",
-        "https://play.google.com/store/books/details/Timothy_Keller_The_Prodigal_Prophet?id=uRpPDwAAQBAJ",
-        "https://play.google.com/store/books/details/Timothy_Keller_Rediscovering_Jonah?id=dcXdDwAAQBAJ",
+urls = ["https://play.google.com/store/books/details/Rod_Dreher_Live_Not_by_Lies?id=JqfiDwAAQBAJ&gl=DE",
+        "https://play.google.com/store/books/details/Timothy_Keller_Romans_1_7_For_You?id=wwPPDwAAQBAJ&gl=DE",
+        "https://play.google.com/store/books/details/Timothy_Keller_Romans_8_16_For_You?id=uwTPDwAAQBAJ&gl=DE",
+        "https://play.google.com/store/books/details/Timothy_Keller_Galatians_For_You?id=7QTPDwAAQBAJ&gl=DE",
+        "https://play.google.com/store/books/details/Timothy_Keller_Judges_For_You?id=XQTPDwAAQBAJ&gl=DE",
+        "https://play.google.com/store/books/details/Timothy_Keller_On_Marriage?id=i2asDwAAQBAJ&gl=DE",
+        "https://play.google.com/store/books/details/Timothy_Keller_The_Prodigal_Prophet?id=uRpPDwAAQBAJ&gl=DE",
+        "https://play.google.com/store/books/details/Timothy_Keller_Rediscovering_Jonah?id=dcXdDwAAQBAJ&gl=DE",
         "https://play.google.com/store/books/details/Henry_Cloud_Boundaries_Updated_and_Expanded_Editio?id=-YVDDgAAQBAJ&gl=DE",
         "https://play.google.com/store/books/details/Gary_Thomas_Sacred_Marriage?id=_txoCgAAQBAJ&gl=DE",
-        "https://play.google.com/store/books/details/Debra_Fileta_Love_in_Every_Season?id=-eCwDwAAQBAJ&gl=DE"]
-# "https://play.google.com/store/books/details/Timothy_Keller_On_Birth?id=i3ypDwAAQBAJ",
-# "https://play.google.com/store/books/details/Timothy_Keller_The_Prodigal_Prophet?id=NX9ODwAAQBAJ",
-# "https://play.google.com/store/books/details/Timothy_Keller_On_Marriage?id=znupDwAAQBAJ=DE"]
+        "https://play.google.com/store/books/details/Debra_Fileta_Love_in_Every_Season?id=-eCwDwAAQBAJ&gl=DE",
+        "https://play.google.com/store/books/details/Timothy_Keller_On_Birth?id=i3ypDwAAQBAJ&gl=DE",
+        "https://play.google.com/store/books/details/Timothy_Keller_The_Prodigal_Prophet?id=NX9ODwAAQBAJ&gl=DE",
+        "https://play.google.com/store/books/details/Timothy_Keller_On_Marriage?id=znupDwAAQBAJ&gl=DE"]
 
 for url in urls:
     livenbl = requests.get(url)
@@ -35,13 +33,7 @@ for url in urls:
     title = re.findall('[A-Z]\w*', temp)
     ct = datetime.datetime.now()
     key += 1
-    tuple = (key, "\t", preis["content"], "\t", title[0],"\t", ct, '\n')
+    tuple = (key, "\t", preis["content"], "\t", title[0], "\t", ct, '\n')
     string = convertTuple(tuple)
 
-    with open("/home/debrrvm/Documents/scrapedBookPrices.txt", "a") as file:
-        #file.write(key, "\t", preis["content"], "\t", title[0], '\n')
-        file.write(string)
-        file.close()
-
-
-time.sleep(3600)
+    print(string)
